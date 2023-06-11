9be65c5b05f47d5709a9b26630b1bf8e8f73bfee
Szabó_Gergő <gergo.szabo93@gmail.com>
2023-06-11 11:13:52 +0200

    modified

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld{
 	private int a;
 	public static void main (String[] args) {
 			
-			System.out.println("Hello World2!");
+			System.out.println("Hello World2 nice..!");
 		
 		
 	}
