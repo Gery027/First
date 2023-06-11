24dd82e9874ea9fe33dcde1c79d0e460907b2704
Szabó_Gergő <gergo.szabo93@gmail.com>
2023-06-11 11:08:17 +0200

    kíváncsi vagyok ez most milyen lesz

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
 public class HelloWorld{
+	private int a;
 	public static void main (String[] args) {
-			System.out.println("Hello World!");
+			
+			System.out.println("Hello World2!");
 		
 		
 	}
